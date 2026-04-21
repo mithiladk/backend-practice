134a63849acdbd4533b36c2b2c74372df5ea6494
mithiladk <mithila.deekay@gmail.com>
2026-04-21 11:31:37 +0530

    written utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asynHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next);
+  };
+};
+
+export { asynHandler };
+
+// const asynHandler = () => {}
+// const asynHandler = (func) =>  () => {}
+// const asynHandler = (func) => async () => {}
+
+// const asynHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
